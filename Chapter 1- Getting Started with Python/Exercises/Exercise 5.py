f8abced4179689474ec6f12b5d74d42242f5c498
blomikii <samanthariannemendoza@gmail.com>
2023-09-27 22:41:52 +0400

    Exercise 5.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
@@ -4,7 +4,6 @@ print("Try to look for the area of a circle.")
 
 pi = 3.14
 radius = float(input("What is the radius of the circle:"))
-
 area = pi*radius*radius
 
 print(f"The area of the circle is = {area:.2f}")
