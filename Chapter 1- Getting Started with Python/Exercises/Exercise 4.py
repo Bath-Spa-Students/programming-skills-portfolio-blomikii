0cd232fc9ad2129142197a2a832d73fb2ab4c88b
blomikii <samanthariannemendoza@gmail.com>
2023-09-27 22:39:53 +0400

    Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -0,0 +1,9 @@
+# Exercise 4
+
+var_1 = "Hey there! "
+var_2 = "How are you? "
+var_3 = "Have a great day!"
+
+result = "{}{}{}".format(var_1, var_2, var_3)
+
+print(result)
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
@@ -0,0 +1,10 @@
+#Exercise 5
+
+print("Try to look for the area of a circle.")
+
+pi = 3.14
+radius = float(input("What is the radius of the circle:"))
+
+area = pi*radius*radius
+
+print(f"The area of the circle is = {area:.2f}")
