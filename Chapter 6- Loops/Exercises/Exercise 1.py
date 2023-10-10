8810e6d60b83b894ea71fc393dd853c107da8467
blomikii <samanthariannemendoza@gmail.com>
2023-10-10 21:50:53 +0400

    Exercise 1.py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -2,3 +2,16 @@
 
 ''' Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. As they enter each topping, print a message saying you’ll add that topping to their pizza. '''
 
+toppings = []
+
+while True:
+    topping = input("What toppings would you like on your pizza? To finish, enter 'quit': ")
+    if topping != 'quit':
+        toppings.append(topping)
+        print(f"{topping} has been added to your pizza.")
+    else:
+        break
+
+print("Your pizza is ready with the followings: ")
+for topping in toppings:
+    print(topping)
---
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -0,0 +1,10 @@
+# Exercise 3
+
+''' Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.'''
+
+def make_shirt(size, text):
+    print(f"The size of my t-shirt is {size}.")
+    print(f'It has a message saying, "{text}".')
+
+make_shirt('extra small', 'Roacwk On')
+make_shirt(size='medium', text='You light up my world :3')
