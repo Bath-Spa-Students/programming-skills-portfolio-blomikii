9f2521e8b9be7ebde461af51d7d6c38130e1a397
blomikii <samanthariannemendoza@gmail.com>
2023-10-18 10:14:56 +0400

    Exercise 5.py

--- Chapter 3- Data Structures/Exercises/Exercise 5.py
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -7,9 +7,14 @@ print (guests[1], "you're invited to my dinner.")
 print (guests[2], "you're invited to my dinner.")
 print (guests[3], "you're invited to my dinner.")
 
+print()
+
 print(guests[0], "can't come anymore.")
 del(guests[0])
+print("A new guest has been invited!")
 guests.insert(0, 'Katkat')
+
+print()
 
 print (guests[0], "you're invited to my dinner.")
 print (guests[1], "you're invited to my dinner.")
