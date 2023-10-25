7488ceda9fb8cdecd48b3523d82fd0d01fc51ae1
blomikii <samanthariannemendoza@gmail.com>
2023-10-25 10:35:06 +0400

    Chapter 6 Exercises.py

--- Chapter 6- Loops/Exercises/Exercise 3_Infinity.py
+++ Chapter 6- Loops/Exercises/Exercise 3_Infinity.py
@@ -2,7 +2,7 @@
 
 ''' Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.) '''
 
-##############################################################################################################
+###############################################################################################################
 
 while True:
-    print("hehe")
+    print("This loop is infinite.")
